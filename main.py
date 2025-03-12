d1378c1499c20af2c1957ff3758b5a25b655804b
Nikolai Tønder <nikolet@stud.ntnu.no>
2025-03-13 00:25:39 +0100

    will now run without importing everything, so it will run much smoother and on any pc

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from runCPU import trainModelCpu
-from runGPU import trainModelGpu , testModelGpu
-
-
+import importlib
 
 def main_menu():
     while True:
@@ -15,17 +12,29 @@ def main_menu():
         choice = input("Enter your choice (1-5): ")
 
         if choice == "1":
-            trainModelCpu.train_model_cpu()
+            # Dynamically import the train_model_cpu from the runCPU folder
+            runCPU = importlib.import_module("runCPU.trainModelCpu")
+            runCPU.train_model_cpu()
+        
         elif choice == "2":
+            # Add your code for testing the model on CPU if needed
             break
+        
         elif choice == "3":
-            trainModelGpu.train_model_gpu()
+            # Dynamically import the train_model_gpu from the runGPU folder
+            runGPU = importlib.import_module("runGPU.trainModelGpu")
+            runGPU.train_model_gpu()
+        
         elif choice == "4":
-           path =  input("Enter the path of the image you want to test: ")
-           testModelGpu.test_model(path)
+            # Dynamically import the test_model_gpu from the runGPU folder
+            path = input("Enter the path of the image you want to test: ")
+            runGPU = importlib.import_module("runGPU.testModelGpu")
+            runGPU.test_model_gpu(path)
+        
         elif choice == "5":
             print("Exiting program...")
             break
+        
         else:
             print("Invalid choice! Please enter a number between 1 and 5.")
 
